fe58d942566b7d0bf9504c10e38ecd6b0820a05e
Zebadiah <zebadiahww@gmail.com>
2020-08-26 10:29:51 -0600

    created Entry Model and started EntryController

---
+++ Journal352/Journal352/Controllers/EntryController.swift
@@ -0,0 +1,15 @@
+//
+//  EntryController.swift
+//  Journal352
+//
+//  Created by Zebadiah Watson on 8/26/20.
+//  Copyright © 2020 Zebadiah Watson. All rights reserved.
+//
+
+import Foundation
+
+
+class EntryController {
+    
+    //TODO add shared instance, SOT, and CRUD functions
+}
---
+++ Journal352/Journal352/Model/Entry.swift
@@ -0,0 +1,20 @@
+//
+//  Entry.swift
+//  Journal352
+//
+//  Created by Zebadiah Watson on 8/26/20.
+//  Copyright © 2020 Zebadiah Watson. All rights reserved.
+//
+
+import Foundation
+
+class Entry {
+    
+    var title: String
+    var body: String
+    
+    init(title: String, body: String) {
+        self.title = title
+        self.body = body
+    }
+}
