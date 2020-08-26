04d9ecc28a2d13b6dc79aac8e5624d649289fb19
Zebadiah <zebadiahww@gmail.com>
2020-08-26 10:48:18 -0600

    added equatable to model, should be good to go on Controller

--- Journal352/Journal352/Model/Entry.swift
+++ Journal352/Journal352/Model/Entry.swift
@@ -18,3 +18,9 @@ class Entry {
         self.body = body
     }
 }
+
+extension Entry: Equatable {
+    static func == (lhs: Entry, rhs: Entry) -> Bool {
+        return lhs.title == rhs.title && lhs.body == rhs.body
+    }
+}
